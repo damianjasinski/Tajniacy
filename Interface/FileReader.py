a1e72fe8822c279ed10cddb5f671c75d0952ab13
Szymon Zajac <moniukins@gmail.com>
2021-06-07 16:12:23 +0200

    Added FileReader

---
+++ Interface/FileReader.py
@@ -0,0 +1,29 @@
+import sys
+
+class FileReader:
+    def __init__(self):
+        self.words = tuple()
+        self.read = False
+    def read_file(self, filename):
+        with open(filename, "rt") as file:
+            wordsTable = list()
+            for line in file:
+                line = line.strip()
+                if line == "":
+                    continue
+                if line[0] == "#":
+                    continue
+                wordsTable.append(line)
+            self.words = tuple(wordsTable)
+        self.read = True
+    def get_words(self):
+        if self.read:
+            return self.words
+        else:
+            return None
+
+if __name__ == "__main__":
+    fr = FileReader()
+    fr.read_file("Interface/words.txt")
+    for w in fr.get_words():
+        print(w)
