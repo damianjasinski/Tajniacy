f845c71acc38110c5fcbb56aa5ee4a054c5899ce
Piotr Krupa <piotrkrupa06@gmail.com>
2021-06-20 23:17:31 +0200

    Add packets test code

--- server/Server.py
+++ server/Server.py
@@ -1,6 +1,30 @@
+import socket
+import threading
+
+from shared.PacketHandler import PacketHandler
+from shared.c2s.HandshakeC2S import HandshakeC2S
+
+class Server():
+    def __init__(self, port):
+
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serverSocket.bind((socket.gethostname(), 4123))
+        self.serverSocket.listen(8)
+
+    def run(self):
+        while True:
+            clientSocket, address = self.serverSocket.accept()
+            print(f"Connection from {address} hasd been established")
+
+
+def handleAsdf(packet: HandshakeC2S):
+    print(f"HandshakeC2S: {packet.name}")
 
 
 def main():
-    print("server start")
-
+    # server = Server()
+    handler = PacketHandler()
+    handler.register(HandshakeC2S, handleAsdf)
+    handler.handle(HandshakeC2S("asdf"))
+    
 main()
---
+++ shared/PacketHandler.py
@@ -0,0 +1,11 @@
+import socket
+
+class PacketHandler():
+    def __init__(self) -> None:
+        self.handlers = {}
+
+    def register(self, packet_type, func):
+        self.handlers[hash(packet_type)] = func
+
+    def handle(self, packet):
+        self.handlers[hash(packet.__class__)](packet)
